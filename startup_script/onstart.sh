53d3f2fdefdf842ce146958e71827ff37a6e86c3
daniel ortega <hazelwood69@gmail.com>
2016-04-22 15:26:55 +0200

    change init script

--- startup_script/onstart.sh
+++ startup_script/onstart.sh
@@ -5,6 +5,7 @@ SIMPLEHTML="<html>
 <body>Hello World!!!</body>
 </html>
 "
+logger "Azure Deployment: onstart.sh"
 cd /tmp
 mkdir nginx_up && mkdir nginx_up/static-content
 cd nginx_up
@@ -15,10 +16,10 @@ echo "$SIMPLEHTML" > static-content/helloworld.html
 touch Dockerfile
 echo "$DOCKERFILECONTENT" > Dockerfile
 
-docker pull nginx
-
 if ! docker inspect -f {{.State.Running}} solvinity >/dev/null 2>&1; then
+		docker build -t nginx .
         docker run --name solvinity-nginx -d -p 80:80 solvinity-nginx
+        logger "Azure Deployment: run nginx"
 else logger "From Azure Deployment: Container already up nothing to do"
 fi
 
