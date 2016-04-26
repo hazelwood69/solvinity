48cb334261c1ea7873570f4d99724eb86146b9b9
daniel ortega <hazelwood69@gmail.com>
2016-04-26 14:57:49 +0200

    change docker img

--- startup_script/onstart.sh
+++ startup_script/onstart.sh
@@ -1,13 +1,9 @@
 logger "Azure Deployment: onstart.sh"
 cd /tmp
 mkdir docker && cd docker
-docker pull nginx
+
 if ! docker inspect -f {{.State.Running}} solvinity >/dev/null 2>&1; then
-		docker build -t nginx .
-        docker run --name solvinity-nginx -d -p 80:80 solvinity-nginx
+		docker run --name solvinity-nginx -P -d nginx fcd1fb01b14557c7c9d991238f2558ae2704d129cf9fb97bb4fadf673a58580d
         logger "Azure Deployment: run nginx"
 else logger "From Azure Deployment: Container already up nothing to do"
 fi
-
-cd /
-rm -Rf /tmp/docker
