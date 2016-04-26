3d28cd50950fc26275f5b264c454405b7a17d8c6
daniel ortega <hazelwood69@gmail.com>
2016-04-26 14:30:47 +0200

    final test

--- startup_script/onstart.sh
+++ startup_script/onstart.sh
@@ -1,18 +1,13 @@
-DOCKERFILECONTENT="FROM nginx
-COPY static-content /usr/share/nginx/html"
-
+logger "Azure Deployment: onstart.sh"
 cd /tmp
-mkdir nginx_up && mkdir nginx_up/static-content
-cd nginx_up
-git clone https://github.com/BlackrockDigital/startbootstrap-grayscale.git static-content
-touch Dockerfile
-echo "$DOCKERFILECONTENT" > Dockerfile
-
-if ! docker inspect -f {{.State.Running}} grayscale >/dev/null 2>&1; then
-        docker build -t grayscale-nginx .
-        docker run --name grayscale -d -p 80:80 grayscale-nginx
-else logger "From Azure Deployment: Container grayscale already up nothing to do"
+mkdir docker && cd docker
+docker pull nginx
+if ! docker inspect -f {{.State.Running}} solvinity >/dev/null 2>&1; then
+		docker build -t nginx .
+        docker run --name solvinity-nginx -d -p 80:80 solvinity-nginx
+        logger "Azure Deployment: run nginx"
+else logger "From Azure Deployment: Container already up nothing to do"
 fi
 
 cd /
-rm -Rf /tmp/nginx_up
+rm -Rf /tmp/docker
